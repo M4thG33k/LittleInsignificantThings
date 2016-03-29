6f8b6b42c28233aa4bd3fe26be1ade258215d5d4
M4thG33k <pepethemathgeek@gmail.com>
2016-03-29 14:14:30 -0500

    Merged to master

--- src/main/java/com/m4thg33k/lit/core/proxy/CommonProxy.java
+++ src/main/java/com/m4thg33k/lit/core/proxy/CommonProxy.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
+
 public class CommonProxy {
 
     public void preInit(FMLPreInitializationEvent e)
